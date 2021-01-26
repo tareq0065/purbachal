08bec5ce1adfcc554cd82d099211215743b07b40
Tareq Aziz <tareqaziz0065@gmail.com>
2021-01-27 00:09:00 +0600

    Added footer
    Added footer

--- components/HomeLayout.js
+++ components/HomeLayout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Layout, Menu, Breadcrumb } from 'antd';
+import Link from "next/link";
 const { Header, Content, Footer } = Layout;
 const HomeLayout = (props) => {
     return (
@@ -13,6 +14,13 @@ const HomeLayout = (props) => {
             <Content className="site-layout">
                 {props.children}
             </Content>
+            <div className="footer">
+                <div className="footer-menu">
+                    <Link href="about"><a>About</a></Link>
+                    <Link href="terms"><a>Terms of use</a></Link>
+                    <Link href="privacy"><a>Privacy & policy</a></Link>
+                </div>
+            </div>
         </Layout>
     )
 }
---
+++ pages/privacy.js
@@ -0,0 +1,90 @@
+import React from "react";
+import HomeLayout from "../components/HomeLayout";
+
+const Privacy = (props) => {
+    return (
+        <HomeLayout>
+            <div className="content-container">
+                <h1 className="p1">Privacy Policy</h1>
+                <p className="p2">&nbsp;Below we set out our privacy policy which will administrate the way in which we
+                    process any personal information that you provide to us. We will inform you if the way in which we
+                    process your information is to change at any time.&nbsp;</p>
+                <p className="p2">You can access our home page and browse our site without disclosing your personal data
+                    save information collected by cookies that we use.</p>
+                <p className="p1">Who may process data</p>
+                <p className="p2">Your personal information (which includes your name, address, and any other details
+                    you provide to us which concern you as an individual) may be processed both by us and by other
+                    companies within our group. Each of the companies authorized to process your information as
+                    mentioned above will do so in accordance with this privacy policy.</p>
+                <p className="p1">Purpose of processing</p>
+                <p className="p2">We will use your information for the purpose of fulfilling service orders placed by
+                    you, processing any other transactions authorized or made by you with us, informing you of special
+                    offers, and providing other marketing information to you which we think you may find of interest,
+                    undertaking services or customer research/development.</p>
+                <p className="p1">Disclosure of information</p>
+                <p className="p2">In the unlikely event that a liquidator, administrator or receiver is appointed over
+                    us or all or any part of our assets that insolvency practitioner may transfer your information to a
+                    third party purchaser of the business provided that purchaser undertakes to use your information for
+                    the same purposes as set out in this policy. We undertake not to provide your personal information
+                    to third parties save in accordance with this policy. Your information will not be disclosed to
+                    government or local authorities or other government institutions save as required by law or other
+                    binding regulations.</p>
+                <p className="p1">Cookies</p>
+                <p className="p2">We may send a small file to your computer when you visit our website. This will enable
+                    us to identify your computer, track your behavior on our website, and to identify your particular
+                    areas of interest so as to enhance your future visits to this website. We may use cookies to collect
+                    and store personal data and we link information stored by cookies with personal data you supply to
+                    us. Save for the use of cookies, we do not automatically log data or collect data save for
+                    information you specifically provide to us. You can set your computer browser to reject cookies but
+                    this may preclude your use of certain parts of this website.</p>
+                <p className="p2">Third-party vendors, including Google, use cookies to serve ads based on a user's
+                    prior visits to your website. Google's use of the DART cookie enables it and its partners to serve
+                    ads to your users based on their visit to your sites and/or other sites on the Internet. Users may
+                    opt-out of the use of the DART cookie by visiting the&nbsp;advertising opt-out page.</p>
+                <p className="p1">Security measures</p>
+                <p className="p2">We have implemented security policies, rules, and technical measures to protect the
+                    personal data that we have under our control from unauthorized access, improper use, and disclosure,
+                    unauthorized destruction, or accidental loss.</p>
+                <p className="p1">Access to information</p>
+                <p className="p2">You may ask us whether we are keeping personal information about you by emailing us
+                    via the Contact page. We may ask for proof of your identity before providing any information and
+                    reserve the right to refuse to provide the information requested if identity is not established.</p>
+                <p className="p1">Copyright</p>
+                <p className="p2">All website design, text, graphics, the selection and arrangement thereof are
+                    Copyright &copy; 2021, purbachalplot.com, ALL RIGHTS RESERVED.</p>
+                <p className="p1">Trademarks</p>
+                <p className="p2">purbachalplot.com is a trademark of Purbachal Plot Limited and may be registered in
+                    certain parts of the world.</p>
+                <p className="p1">Disclaimer of Warranty and Liability</p>
+                <p className="p2">The following provisions may be curtailed or disallowed by the laws of certain
+                    jurisdictions. In such case, the terms hereof are to be read as excluding or limiting such term so
+                    as to satisfy such law.</p>
+                <p className="p2">We do not represent or warrant that the information accessible via this website is
+                    accurate, complete or current. We have no liability whatsoever in respect of any use which you make
+                    of such information.</p>
+                <p className="p2">The information provided on this website has not been written to meet your individual
+                    requirements and it is your sole responsibility to satisfy yourself prior to ordering any products
+                    or services from us that are suitable for your purposes.</p>
+                <p className="p2">Whilst we make all reasonable attempts to exclude viruses from the website, we cannot
+                    ensure such exclusion and no liability is accepted for viruses. Thus, you are recommended to take
+                    all appropriate safeguards before downloading information or images from this website.</p>
+                <p className="p2">All warranties, express or implied, statutory or otherwise are hereby excluded.</p>
+                <p className="p2">Neither we nor any of our employees or affiliated entities will be liable for any kind
+                    of damages and howsoever arising including, without limitation, loss of profits, compensatory,
+                    consequential, direct, exemplary, incidental, indirect, punitive or special, damages or any
+                    liability which you may have to a third party, even if we have been advised of the possibility of
+                    such loss.</p>
+                <p className="p2">We are not responsible for the direct or indirect consequences of you linking to any
+                    other website from this website.</p>
+                <p className="p1">Consent and inquiries</p>
+                <p className="p2"><span className="s1"><strong>In order to access the information on this website, you must signal acceptance of the terms and disclaimer set out above. If you do not accept any of these terms, leave this website now.&nbsp;</strong></span>If
+                    you have any queries or unease about our privacy policy or the way in which we are handling personal
+                    data, please contact us through our Contact page. If at any time you wish us to cease processing
+                    your information, please send a message to us and insert "unsubscribe" as the subject heading.</p>
+                <br/><br/><br/><br/><br/><br/>
+            </div>
+        </HomeLayout>
+    )
+}
+
+export default Privacy;
