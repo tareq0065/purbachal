c2f8a75f24e91faf92e6916a1bc9810bd3769d79
Tareq Aziz <tareqaziz0065@gmail.com>
2021-02-05 03:59:04 +0600

    Updated bottom button
    Updated bottom button

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,7 +2,7 @@ export default {
     openGraph: {
         type: 'website',
         locale: 'en_IE',
-        site_name: 'পূর্বাচল প্লট',
+        site_name: 'Purbachal Plot',
     },
     twitter: {
         handle: '@handle',
